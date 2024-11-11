47d06d68546963863332986f440b53ab81bac610
sourcengd <basharseed126@gmail.com>
2024-11-11 22:42:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -33,16 +33,16 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/sourcengd/music",
+    "https://github.com/sourcengd/Music-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-CHANNEL_NAME = getenv("CHANNEL_NAME", "تحديثات ماغي ")
-CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/QREAX22")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/QREAX22")
+CHANNEL_NAME = getenv("CHANNEL_NAME", "تحديثات ميلا ")
+CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/m0_ea")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/m0_ea")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
