0d8b0f0b22f4d9bc5b0c36fcb8699c22220cf9e4
sourcengd <basharseed126@gmail.com>
2024-11-15 21:00:32 +0300

    Update play.py

--- ZeMusic/plugins/play/play.py
+++ ZeMusic/plugins/play/play.py
@@ -33,7 +33,7 @@ Nem = config.BOT_NAME + " شغل"
             "شغل",
             Nem,
             "play",
-            "vplay",
+            "بث",
             "cplay",
             "cvplay",
             "playforce",
